73780c9916cab76e4414cef1ca106dc5e249e5f1
Paulo Junior <100033559+paulojrc52@users.noreply.github.com>
2023-03-01 00:09:53 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,6 @@ const router = require('express').Router()
 
 const ProductsController = require('../controllers/products')
 
-// VERBOS HTTP (4 TIPOS)
-// GET - Obter dados
-// POST - Enviar/Receber dados
-// PUT - Atualizar dados
-// DELETE - Remover dados
-
-
 router.get('/products/:id?', ProductsController.get)
 router.post('/products', ProductsController.post)
 router.put('/products/:id', ProductsController.put)
